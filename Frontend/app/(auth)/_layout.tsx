c3ffd152168da72d7b0ff3244af3ce11e05ef2a5
HimayaFernando2001 <himayafernando2001@gmail.com>
2026-05-06 13:43:36 +0530

    Delete Frontend/app directory

--- Frontend/app/(auth)/_layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Stack } from 'expo-router';
-
-export default function AuthLayout() {
-  return (
-    <Stack screenOptions={{ headerShown: false, contentStyle: { backgroundColor: '#1A1A2E' } }}>
-      <Stack.Screen name="login" />
-    </Stack>
-  );
-}
